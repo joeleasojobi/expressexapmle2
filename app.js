48cd08716ff6f40e949df6ec2cd5bdd002bb4fbb
joeleasojobi <joeleasojobi@gmail.com>
2022-04-14 21:01:50 +0530

    completed

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.get('/firstpage',(req,res)=>{
     res.send("This is the first page")
 })
 
-app.listen(3000,()=>{
+app.listen( process.env.PORT ||3000,()=>{
     console.log("Server Started");
 })
